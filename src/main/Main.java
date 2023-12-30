1ca3140bf159adabf009bfe5608cacf1799ed0fb
unknown <frederickchandra52@gmail.com>
2023-12-30 10:25:18 +0700

    nambah menu

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,20 +1,45 @@
 package main;
 
-import java.sql.SQLException;
 import java.util.Scanner;
 
-import model.Connect;
+import view.login;
 
 public class Main {
-	Scanner scan = new Scanner(System.in);
-	Connect con = Connect.getConnection();
-	public Main() {
-		try {
-			con.AddMenu("INSERT INTO msbranch(LocationId, BranchName) VALUES (1, 'LaperAh Tanjung Priuk');");
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	public static Scanner scan = new Scanner(System.in);
+	
+	public static void cls() {
+		for (int i = 0; i <50; i++) {
+			System.out.println();
 		}
+	}
+	
+	public Main() {
+		int Choice = 0;
+		do {
+			System.out.println("LaperAh RestoManagement");
+			System.out.println("=======================");
+			System.out.println("1. Login");
+			System.out.println("2. Register");
+			System.out.println("3. Exit");
+			System.out.print(">> ");
+			try {
+				Choice = scan.nextInt();
+				scan.nextLine();
+				switch (Choice) {
+				case 1:
+					login.loginPage();
+					break;
+
+				case 2:
+					
+					break;
+				}
+			} catch (Exception e) {
+				scan.nextLine();
+			}
+			
+		} while (Choice != 3);
+		
 	}
 	
 
